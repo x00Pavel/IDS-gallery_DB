6c68f5a0fea18fba8d18473d9d677380da0e43ea
x00Pavel <xyadlo00@stud.fit.vutb.cz>
2020-04-04 14:21:03 +0200

    new selcetions and insertions

--- selections.sql
+++ selections.sql
@@ -11,13 +11,13 @@ WHERE P1.fk_id_adress = P2.fk_id_adress
      
      
         
--- Ktery pronajmateli platily za stejny poplatek?
-SELECT DISTINCT
-    P.*, st1.suma, pop.zbivajici_suma
+-- Ktery pronajmateli platily za jeden popla?
+-- uvest vsechny transakci za ktere placlili ne jednou 
+SELECT distinct
+   pop.id_cislo_poplatku ,P.*, st1.suma
 FROM
     PRONAJIMATEL P, SEZNAM_TRANSAKCI ST1, SEZNAM_TRANSAKCI ST2, POPLATEK POP
 WHERE
-    
     ST1.id_transakci != ST2.id_transakci
     AND
     ST1.id_pronajmatel = ST2.id_pronajmatel
@@ -26,4 +26,18 @@ WHERE
     AND
     pop.id_cislo_poplatku = st1.id_poplatek
     AND
-    ST1.id_pronajmatel = P.id_pronajimatel;
+    ST1.id_pronajmatel = P.id_pronajimatel
+order by 
+    id_pronajimatel;
+    
+-- seznam del v oplacene ekspozici
+
+select *
+from delo d
+where 
+d.id_dela = 3 and 
+exists(
+    select * 
+    from SEZNAM_DEL_V_EXPOZICI SD
+    where sd.id_delo = d.id_dela
+)  
